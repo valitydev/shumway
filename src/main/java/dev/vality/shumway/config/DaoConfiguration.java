cef48eca0e887ba2de9d3ca75ee23f0189b09ef9
Kostya Struga <kennny007@yandex.ru>
2023-05-16 16:05:36 +0300

    Fix pool config^ for use config from file

--- src/main/java/dev/vality/shumway/config/DaoConfiguration.java
+++ src/main/java/dev/vality/shumway/config/DaoConfiguration.java
@@ -6,37 +6,13 @@ import dev.vality.shumway.dao.PostingPlanDao;
 import dev.vality.shumway.dao.impl.AccountDaoImplNew;
 import dev.vality.shumway.dao.impl.AccountReplicaDaoImpl;
 import dev.vality.shumway.dao.impl.PostingPlanDaoImpl;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DaoConfiguration {
-
-    @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource(@Value("${spring.datasource.url}") String url) {
-        return DataSourceBuilder
-                .create()
-                .url(url)
-                .build();
-    }
-
-    @Bean(name = "replicaDatasource")
-    @ConfigurationProperties(prefix = "spring.replica-datasource")
-    public DataSource replicaDataSource(@Value("${spring.replica-datasource.url}") String url) {
-        return DataSourceBuilder
-                .create()
-                .url(url)
-                .build();
-    }
 
     @Bean(name = "accountDao")
     public AccountDao accountDao(DataSource dataSource) {
@@ -44,7 +20,7 @@ public class DaoConfiguration {
     }
 
     @Bean(name = "replicaDao")
-    public AccountReplicaDao replicaDao(@Qualifier("replicaDatasource") DataSource dataSource) {
+    public AccountReplicaDao replicaDao(DataSource dataSource) {
         return new AccountReplicaDaoImpl(dataSource);
     }
 
