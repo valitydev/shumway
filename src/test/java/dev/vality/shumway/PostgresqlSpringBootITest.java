cafce861ac8df7bc7006a921277fdc9f73d53b4a
f.shimich <fshim.dev@gmail.com>
2025-05-29 15:56:07 +0300

    fix

--- src/test/java/dev/vality/shumway/PostgresqlSpringBootITest.java
+++ src/test/java/dev/vality/shumway/PostgresqlSpringBootITest.java
@@ -2,8 +2,6 @@ package dev.vality.shumway;
 
 import dev.vality.testcontainers.annotations.postgresql.PostgresqlTestcontainerSingleton;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -16,8 +14,6 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @Retention(RetentionPolicy.RUNTIME)
 @PostgresqlTestcontainerSingleton
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@ContextConfiguration(classes = ShumwayApplication.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public @interface PostgresqlSpringBootITest {
 
 }
